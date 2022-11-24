d93eb12b9f623910aac42a43b4e0b408b874a391
Edgar Aparicio Baeza <holamundo@edgarbaeza.mx>
2022-11-24 17:00:46 -0600

    Adding getCurrentPlayers endpoint

--- functions/index.js
+++ functions/index.js
@@ -2,43 +2,52 @@ const functions = require("firebase-functions");
 const axios = require("axios");
 const defaultAvatar = "https://storage.googleapis.com/aoe3-companion.appspot.com/public/flag_random_1x1.png";
 const steamDefaultHash = "fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb";
-
+const cors = require("cors")({origin: true});
 const baseURL = "https://api.steampowered.com";
+const appid = "933110";
 
 exports.steamSummary = functions.https.onRequest((req, res) => {
-  const steamId = req.query.steamId;
-  let playerSummary = null;
+  cors(req, res, () => {
+    const steamId = req.query.steamId;
+    let playerSummary = null;
 
-  res.set("Access-Control-Allow-Origin", "*");
+    axios.get(`${baseURL}/ISteamUser/GetPlayerSummaries/v2/`, {
+      params: {
+        key: process.env.STEAM_KEY,
+        steamids: steamId,
+      },
+    }).then((resp) => {
+      if (resp.data) {
+        const [player] = resp.data.response.players;
 
-  if (req.method === "OPTIONS") {
-    // Send response to OPTIONS requests
-    res.set("Access-Control-Allow-Methods", "GET");
-    res.set("Access-Control-Allow-Headers", "Content-Type");
-    res.set("Access-Control-Max-Age", "3600");
-    return res.status(204).send("");
-  }
+        if (player) {
+          playerSummary = player;
 
-  axios.get(`${baseURL}/ISteamUser/GetPlayerSummaries/v2/`, {
-    params: {
-      key: process.env.STEAM_KEY,
-      steamids: steamId,
-    },
-  }).then((resp) => {
-    if (resp.data) {
-      const [player] = resp.data.response.players;
-
-      if (player) {
-        playerSummary = player;
-
-        if (playerSummary.avatarhash === steamDefaultHash) {
-          playerSummary.avatarfull = defaultAvatar;
+          if (playerSummary.avatarhash === steamDefaultHash) {
+            playerSummary.avatarfull = defaultAvatar;
+          }
         }
       }
-    }
 
-    res.send(playerSummary);
-  }).catch((error) => {
-    return res.status(500).json({error});
+      res.send(playerSummary);
+    }).catch((error) => {
+      return res.status(500).json({error});
+    });
+  });
+});
+
+
+exports.getCurrentPlayers = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    axios.get(`${baseURL}/ISteamUserStats/GetNumberOfCurrentPlayers/v1/`, {
+      params: {
+        key: process.env.STEAM_KEY,
+        appid,
+      },
+    }).then((resp) => {
+      res.send(resp.data.response);
+    }).catch((error) => {
+      return res.status(500).json({error});
+    });
   });
 });
